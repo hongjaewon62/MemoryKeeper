223385a7b1ca3ce469bf57f93e0c5ac01d34c381
hongjaewon62 <hongjaewon0602@gmail.com>
2025-05-19 23:18:23 +0900

    로그인, 회원가입 백엔드 연동

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 
 import InputUI from "../ui/InputUI";
 import Button from "../ui/Button";
+import { useState } from "react";
+import axios from "axios";
 
 const Wrapper = styled.div`
     display: flex;
@@ -47,17 +49,53 @@ const CheckBoxText = styled.span`
 function Login () {
     const navigate = useNavigate();
 
+    const [id, setId] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post("/api/users/login", null, {
+                params: {
+                    username: id,
+                    password: password
+                }
+            });
+
+            navigate("/home");
+        } catch(error) {
+            alert("로그인 실패");
+        }
+    };
+
     return (
         <Wrapper>
             <LoginText>로그인</LoginText>
             <LoginWrapper>
-                <InputUI type="text" placeholder="아이디를 입력하세요" $width={"80%"} $height={"10%"} $margin={"0 0 25px 0"} $icon={"img/user.png"}/>
-                <InputUI type="password" placeholder="비밀번호를 입력하세요" $width={"80%"} $height={"10%"} $margin={"0 0 25px 0"} $icon={"img/password.png"}/>
+                <InputUI
+                    type="text"
+                    placeholder="아이디를 입력하세요"
+                    $width={"80%"}
+                    $height={"10%"}
+                    $margin={"0 0 25px 0"}
+                    $icon={"img/user.png"}
+                    value={id}
+                    onChange={(e) => setId(e.target.value)}
+                />
+                <InputUI
+                    type="password"
+                    placeholder="비밀번호를 입력하세요"
+                    $width={"80%"}
+                    $height={"10%"}
+                    $margin={"0 0 25px 0"}
+                    $icon={"img/password.png"}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 <CheckBoxDiv>
                     <InputUI type="checkbox" $width={"25px"} $height={"25px"} />
                     <CheckBoxText>아이디 저장</CheckBoxText>
                 </CheckBoxDiv>
-                <Button title={"로그인"} $width={"80%"} $height={"10%"} $margin={"0 0 25px 0"} />
+                <Button title={"로그인"} $width={"80%"} $height={"10%"} $margin={"0 0 25px 0"} onClick={handleLogin}/>
                 <Button title={"회원가입"} $width={"80%"} $height={"10%"} $margin={"0 0 25px 0"} onClick={() => {
                     navigate("/signUp")
                 }} />
--- src/login/SignUp.jsx
+++ src/login/SignUp.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 
 import InputUI from "../ui/InputUI";
 import Button from "../ui/Button";
+import { useState } from "react";
+import axios from "axios";
 
 const Wrapper = styled.div`
     display: flex;
@@ -31,33 +33,77 @@ const LoginText = styled.div`
     margin-bottom: clamp(10px, 20px, 40px);
 `
 
-const CheckBoxDiv = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 80%;
-    justify-content: flex-start;
-    margin-bottom: 25px;
-`
-
-const CheckBoxText = styled.span`
-    font-size: clamp(12px, 1vw, 35px);;
-`
-
 function Login () {
     const navigate = useNavigate();
 
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const handleSignUp = async () => {
+        if(password !== confirmPassword) {
+            alert("비밀번호가 일치하지 않습니다.");
+            return;
+        }
+
+        try {
+            const userData = {
+                username: username,
+                email: email,
+                password: password
+            };
+
+            const response = await axios.post("/api/users/signUp", userData);
+
+            navigate("/login");
+        } catch (error) {
+            alert("회원가입 실패: " + error.response.data);
+        }
+    }
     return (
         <Wrapper>
             <LoginText>회원가입</LoginText>
             <LoginWrapper>
-                <InputUI type="text" placeholder="아이디를 입력하세요" $width={"80%"} $height={"10%"} $margin={"0 0 25px 0"} $icon={"img/user.png"}/>
-                <InputUI type="email" placeholder="이메일을 입력하세요" $width={"80%"} $height={"10%"} $margin={"0 0 25px 0"} $icon={"img/mail.png"}/>
-                <InputUI type="password" placeholder="비밀번호를 입력하세요" $width={"80%"} $height={"10%"} $margin={"0 0 25px 0"} $icon={"img/password.png"}/>
-                <InputUI type="password" placeholder="비밀번호를 한번 더 입력하세요" $width={"80%"} $height={"10%"} $margin={"0 0 25px 0"} $icon={"img/confirmation.png"}/>
-                <Button title={"회원가입"} $width={"80%"} $height={"10%"} $margin={"0 0 25px 0"} onClick={() => {
-                    navigate("/login")
-                }} />
+                <InputUI
+                    type="text"
+                    placeholder="아이디를 입력하세요"
+                    $width={"80%"}
+                    $height={"10%"}
+                    $margin={"0 0 25px 0"}
+                    $icon={"img/user.png"}
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <InputUI
+                    type="email"
+                    placeholder="이메일을 입력하세요"
+                    $width={"80%"}
+                    $height={"10%"}
+                    $margin={"0 0 25px 0"}
+                    $icon={"img/mail.png"}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <InputUI
+                    type="password"
+                    placeholder="비밀번호를 입력하세요"
+                    $width={"80%"} $height={"10%"}
+                    $margin={"0 0 25px 0"}
+                    $icon={"img/password.png"}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <InputUI
+                    type="password"
+                    placeholder="비밀번호를 한번 더 입력하세요"
+                    $width={"80%"} $height={"10%"}
+                    $margin={"0 0 25px 0"}
+                    $icon={"img/confirmation.png"}
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <Button title={"회원가입"} $width={"80%"} $height={"10%"} $margin={"0 0 25px 0"} onClick={handleSignUp}/>
             </LoginWrapper>
         </Wrapper>
     )
